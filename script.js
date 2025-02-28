1c289083a73b3ebc10272aaa51e6874049453d4d
PauloDiney <paulo19martin@gmail.com>
2025-02-28 17:16:41 -0300

    Finalizado parte 1

--- script.js
+++ script.js
@@ -29,3 +29,55 @@ setInterval(() => {
     currentIndex = (currentIndex + 1) % totalItems;
     updateCarousel();
 }, intervalTime);
+
+document.getElementById('btn-prudente').addEventListener('click', function() {
+    document.getElementById('day').innerText = 'TERÇA-FEIRA';
+    document.getElementById('time').innerText = 'HORÁRIO - 18:30H ÁS 19:30H';
+    document.getElementById('description').innerText = 'Para crianças iniciantes até 07 anos';
+    document.getElementById('remove-p').innerText = '*obs - por enquanto só temos turma na terça'
+    toggleSection(true);
+    toggleActiveButton('btn-prudente');
+    updateClassItems('R$ 30,00', 'R$ 30 x 4 = R$ 120', 'R$ 35 x 4 = R$ 140');
+});
+
+document.getElementById('btn-feijo').addEventListener('click', function() {
+    document.getElementById('day').innerText = 'SEXTA-FEIRA';
+    document.getElementById('time').innerText = 'HORÁRIO - 17:00H ÁS 18:00H  18:00H ÁS 19:00H';
+    document.getElementById('remove-p').innerText = '';
+    toggleSection(false);
+    toggleActiveButton('btn-feijo');
+    updateClassItems('R$ 25,00', 'R$ 25 x 4 = R$ 100', '');
+});
+
+function toggleSection(add) {
+    var section = document.getElementById('remove');
+    if (add) {
+        if (!section) {
+            const newSection = document.createElement('section');
+            newSection.id = 'remove';
+            newSection.className = 'clas-itens-1';
+            newSection.innerHTML = `
+                <h2>INDIVIDUAL</h2>
+                <p>R$ 35,00</p>
+                <button>Contato</button>
+            `;
+            document.querySelector('.clas-align').appendChild(newSection);
+        }
+    } else {
+        if (section) {
+            section.remove();
+        }
+    }
+}
+
+function toggleActiveButton(activeButtonId) {
+    document.getElementById('btn-prudente').classList.remove('active');
+    document.getElementById('btn-feijo').classList.remove('active');
+    document.getElementById(activeButtonId).classList.add('active');
+}
+
+function updateClassItems(groupPrice, monthlyPrice1, monthlyPrice2) {
+    document.getElementById('mud-1').innerText = groupPrice;
+    document.querySelector('.mud-2').innerText = monthlyPrice1;
+    document.querySelector('.mud-3').innerText = monthlyPrice2;
+}
